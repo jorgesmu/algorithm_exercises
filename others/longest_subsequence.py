9b5b5b1a3288f05e8d889f43730e1de8fb7c7116
Jorge Smulevici <jsmulevici@gmail.com>
2019-03-23 15:04:42 +0200

    add dynamic programming cache to longest subsequence problem

--- others/longest_subsequence.py
+++ others/longest_subsequence.py
@@ -1,16 +1,20 @@
 # Problem taken from: https://www.youtube.com/watch?v=Qf5R-uYQRPk&index=4&list=PLBZBJbE_rGRU5PrgZ9NBHJwcaZsNpf8yD&t=0s
-
-def lcs(p, q, pi, qi):
-	if pi == -1 or qi == -1:
-		return 0
-	if p[pi] == q[qi]:
-		return 1 + lcs(p, q, pi-1, qi-1)
+# O(pq)
+def lcs(p, q, pi, qi, cache):
+	if cache[qi][pi]:
+		return cache[qi][pi]
+	if pi == 0 or qi == 0:
+		result = 0
+	elif p[pi-1] == q[qi-1]:
+		result =  1 + lcs(p, q, pi-1, qi-1, cache)
 	else:
-		return max(lcs(p, q, pi, qi-1), lcs(p, q, pi-1, qi))
-
+		result = max(lcs(p, q, pi, qi-1, cache), lcs(p, q, pi-1, qi, cache))
+	cache[qi][pi] = result
+	return result
 
 p = 'baterd'
-pi = len(p) - 1
+pi = len(p)
 q = 'abaercd'
-qi = len(q) - 1
-print(lcs(p, q, pi, qi))
+qi = len(q)
+cache = [[None for i in range(len(p)+1)] for j in range(len(q)+1)]
+print(lcs(p, q, pi, qi, cache))
