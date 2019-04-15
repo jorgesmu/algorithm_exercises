d0ba2e7e191f5bc6f75f9540baa4e71f23f3f22e
Jorge Smulevici <jsmulevici@gmail.com>
2019-04-15 22:42:22 +0300

    fix anagrams problem

--- others/anagrams.py
+++ others/anagrams.py
@@ -2,7 +2,7 @@ def anagrams(s1, s2):
 	if len(s1) != len(s2):
 		return False
 	for i in range(len(s1)):
-		if s1[i] != s2[-i+1]:
+		if s1[i] != s2[-i-1]:
 			return False
 	return True
 
@@ -10,3 +10,6 @@ def execute(s1, s2):
 	print('s1: ', s1, ', s2: ', s2, ' anagrams: ', anagrams(s1, s2))
 
 execute('banana', 'ananab')
+execute('oso', 'oso')
+execute('sfs', 'ananab')
+execute('goloso', 'osolog')
