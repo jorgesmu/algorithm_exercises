49198bc3cf17f49ea532bd42ce99994aaeb4cfb2
Jorge Smulevici <jsmulevici@gmail.com>
2019-04-16 21:38:32 +0300

    add swap in place problem

---
+++ others/swap.py
@@ -0,0 +1,16 @@
+# Taken from: https://www.youtube.com/watch?v=DtnH3V_Vjek&list=PLNmW52ef0uwsjnM06LweaYEZr-wjPKBnj&index=30
+
+a = 6 
+b = 5 
+
+# Note that we are not really swapping as ints are inmutable
+# and are passed by value to the swap function
+def swap(a, b):
+	a = a + b
+	b = a - b
+	a = a - b	
+	return a, b
+
+print('swap of a: ', a, ', b: ', b, 'is:')
+a, b = swap(a, b)
+print('a: ', a, ', b: ', b)
