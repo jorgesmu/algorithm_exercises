d41c8473ebced0bf4d9127fc82f2c1c619a4d7dc
Jorge Smulevici <jsmulevici@gmail.com>
2019-04-24 00:58:00 +0300

    add big int modulus problem

---
+++ others/big_int_modulus.py
@@ -0,0 +1,13 @@
+# Taken from: https://www.youtube.com/watch?v=5LCuroQltsc&list=PLNmW52ef0uwsjnM06LweaYEZr-wjPKBnj&index=36
+import math
+
+def modulus(bytes_array, n):
+	modulus = 0
+
+	base_bits = 8
+	for i in bytes_array:
+		modulus = ((modulus<<base_bits) + i) % n
+	return modulus
+
+array = bytes.fromhex('deadbeef')
+print('0x', array, ' modulus 14 is: ', modulus(array, 14))
